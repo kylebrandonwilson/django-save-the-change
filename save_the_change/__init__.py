d53659ed9772a4ac2bde88f3cfc69e95a77afbb4
Kyle Wilson <kyle.wilson@storagecraft.com>
2018-01-16 16:28:09 -0700

    bumping version

--- save_the_change/__init__.py
+++ save_the_change/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 
 
-VERSION = (1, 1, 0)
+VERSION = (1, 1, 1)
 
 __title__ = 'Save The Change'
 __version__ = '.'.join((str(i) for i in VERSION)) # str for compatibility with setup.py under Python 3.
